b9ce3985b8cb1e2f715d1d9c50014c064f325053
Harrison Inocencio <hain222@uky.edu>
2018-05-30 21:10:40 -0400

    Add percentage of bad bases and read length to the interactive mode, and begin preparations for new color code mode based off of a given motif

--- seqColor.py
+++ seqColor.py
@@ -62,7 +62,7 @@ def fasta_print(rec_list):
 		
 			for idx, base in enumerate(rec.seq):
 				wrap_iter += 1
-				base_print(quals[idx], base)
+				qual_base_print(quals[idx], base)
 				if wrap_iter == wrap_count:
 					print()
 					wrap_iter = 0
@@ -86,9 +86,10 @@ def seq_iterator(rec_list):
 		print()
 		exit(0)
 
-# base_print function
-# > prints a base according to an associated quality score
-def base_print(qual_score, base):
+# qual_base_print function
+# > prints a base according to an associated quality score and returns
+# > the given score numerically
+def qual_base_print(qual_score, base):
 	global bad_erange
 	global avg_srange
 	global avg_erange
@@ -96,15 +97,27 @@ def base_print(qual_score, base):
 
 	colorama.init()
 
+	# Calls
+	# 0 = initial
+	# 1 = bad
+	# 2 = average
+	# 3 = good
+	call = 0;
+
 	if qual_score <= bad_erange:
 		print(colorama.Back.RED + base, end='')
 		print(colorama.Style.RESET_ALL, end='')
+		call = 1
 	elif avg_srange <= qual_score <= avg_erange:
 		print(colorama.Back.YELLOW + base, end='')
 		print(colorama.Style.RESET_ALL, end='')
+		call = 2
 	elif qual_score >= good_srange:
 		print(colorama.Back.GREEN + base, end='')
 		print(colorama.Style.RESET_ALL, end='')
+		call = 3
+
+	return call
 
 
 # seq_print function
@@ -113,16 +126,25 @@ def seq_print(rec):
 	global wrap_count
 
 	wrap_iter = 0
+	bad_count = 0
 	quals = rec.letter_annotations["phred_quality"]
 	
 	print("######################################################################\n\n"+rec.id)
 	for idx, base in enumerate(rec.seq):
 		wrap_iter += 1
-		base_print(quals[idx], base)
+
+		call = qual_base_print(quals[idx], base)
+		if call == 1:
+			bad_count += 1
+
 		if wrap_iter == wrap_count:
 			print()
 			wrap_iter = 0
-	print("\n\n######################################################################")
+
+	per_bad = round((bad_count/len(rec.seq) * 100), 1)
+	print("\n\n% Bad: "+str(per_bad))
+	print("Read Length: "+str(len(rec.seq)))
+	print("######################################################################")
 			
 
 # main function
@@ -131,6 +153,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('fastq_input', help='fastq_input HELP')
 	parser.add_argument("-f", "--fasta", help="--fasta HELP", action="store_true")
+	parser.add_argument("-m", "--based-on-motif", help="--based-on-motif HELP", action="store_true")
 	args = parser.parse_args()
 
 	fastq_fid = args.fastq_input
@@ -144,6 +167,8 @@ def main():
 
 
 	#print(seq_recs[0].letter_annotations["phred_quality"])
+	
+	#print(args.based-on-motif)
 
 	if args.fasta:
 		fasta_print(seq_recs)
